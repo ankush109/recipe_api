22eacb26ad0022fc5553949061c58759cc39e03b
Ankush Banerjee <banerjeeankush184@gmail.com>
2024-04-02 01:14:49 +0530

    fixed

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
         'core',
+        'rest_framework',
+        'drf-spectacular',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +71,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'app.wsgi.application'
+
+
+
+
 
 
 # Database
@@ -128,3 +134,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL='core.User'
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
